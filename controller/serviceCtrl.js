a434424648838a7daed5ef0ed9182f7e6ec8a169
Sahinur Islam <infosahinur@gmail.com>
2021-08-15 15:07:17 +0600

    added controller service

---
+++ controller/serviceCtrl.js
@@ -0,0 +1,56 @@
+const { Service } = require('../model/Service')
+
+// Create a new Service
+const createService = async (req, res) => {
+    console.log(req.body);
+    try {
+        const service = await Service.create({
+            text: req.body.text
+        });
+        res.json(service);
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+// Fetch all Service
+const fetchAllService = async (req, res) => {
+    try {
+        const services = await Service.find();
+        res.json(services);
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+// Fetch a Service
+const fetchService = async (req, res) => {
+    try {
+        const service = await Service.findById(req.params.id);
+        res.json(service);
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+// Update a Service
+const updateService = async (req, res) => {
+    try {
+        const service = await Service.findByIdAndUpdate(req.params.id, {
+            service: req.body.text
+        });
+        res.json(todo);
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+// Delete a Service
+const deleteService = async (req, res) => {
+    try {
+        const service = await Service.findByIdAndRemove(req.params.id);
+        res.json(service);
+    } catch (error) {
+        res.json(error);
+    }
+}
